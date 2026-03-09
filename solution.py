41045408390d85ceb6c5456aab248be8bbc80a4d
SomethingFishyy <166070521+SomethingFishyy@users.noreply.github.com>
2026-03-09 13:25:31 -0600

    argument validation

--- solution.py
+++ solution.py
@@ -1,2 +1,19 @@
+
+
 def radix_base(values_to_sort, base):
-    return
+    # validate args
+    # -----
+    if (values_to_sort is None) or (values_to_sort == []):
+        raise ValueError("invalid arguments")
+    if (base < 2):
+        raise ValueError("invalid arguments")
+    
+    try:
+        for element in values_to_sort:
+            element % base
+    except:
+        raise ValueError("invalid list element")
+    # -----
+
+    
+        
